c4ba547819a799bfa366f5c5c4d78afd54194806
Javier <sanchezgarciajavi92@gmail.com>
2020-06-04 16:38:28 +0200

    comments ok

--- src/app/notice-detail/notice-detail.page.ts
+++ src/app/notice-detail/notice-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { AppService } from '../../services/app.service';
 import { Location } from '@angular/common';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-notice-detail',
@@ -10,8 +11,53 @@ import { Location } from '@angular/common';
 })
 export class NoticeDetailPage implements OnInit {
   new: any;
+  comments = [];
 
-  constructor(private activatedRoute: ActivatedRoute, private appService: AppService, private _location: Location) { }
+  get name() {
+    return this.commentForm.get('name');
+  }
+
+  get email() {
+    return this.commentForm.get('email');
+  }
+
+  get comment() {
+    return this.commentForm.get('comment');
+  }
+
+  public errorMessages = {
+    name: [
+      { type: 'required', message: 'Name is required' },
+      { type: 'minlength', message: 'Minimum 2 characters is required' },
+      { type: 'maxlength', message: 'Maximum 30 characters' },
+    ],
+    email: [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Please enter a valid email address' }
+    ],
+    comment: [
+      { type: 'required', message: 'Comment is required' },
+      { type: 'minlength', message: 'Minimum 3 characters is required' },
+      { type: 'maxlength', message: 'Maximum 150 characters' },
+    ]
+  };
+
+  commentForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(2), Validators.max(30)]],
+    email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/)]],
+    comment: ['', [Validators.required, Validators.minLength(3), Validators.max(150)]]
+  });
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private appService: AppService,
+    private _location: Location,
+    private formBuilder: FormBuilder
+  ) { }
+
+  sendComment() {
+    this.comments.push(this.commentForm.value);
+    console.log(this.comments);
+  }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(res => {
